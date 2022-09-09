d9ce0fcf8e66ef9ad5fc34f0d6237b43abb2fba6
unknown <cteds_16>
2022-09-09 19:36:21 -0300

    Set up the project

---
+++ luizeduardosantos-d3-avaliacao/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace luizeduardosantos_d3_avaliacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            while (true)
+            {
+                Console.WriteLine("Escolha uma opcao:\n");
+                Console.WriteLine("1 - acessar\n");
+                Console.WriteLine("2 - cancelar\n");
+
+                string opcao = Console.ReadLine();
+
+                if (opcao == "2" || opcao == "cancelar") break;
+
+                else if (opcao == "1" || opcao == "acessar")
+                {
+                    Console.WriteLine("Digite seu email\n");
+                    string email = Console.ReadLine();
+
+                    Console.WriteLine("Digite sua senha\n");
+                    string senha = Console.ReadLine();
+
+
+
+                }
+
+                else
+                {
+                    Console.WriteLine("opcao invalida");
+                }
+
+            }
+        }
+    }
+}
