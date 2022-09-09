77d955eb28d3b27d3366b6b6cd0e5d72a87d69b3
unknown <cteds_16>
2022-09-09 19:59:06 -0300

    Update Program.cs

--- luizeduardosantos-d3-avaliacao/Program.cs
+++ luizeduardosantos-d3-avaliacao/Program.cs
@@ -13,18 +13,38 @@
                 Console.WriteLine("2 - cancelar\n");
 
                 string opcao = Console.ReadLine();
+                string opcao2 = string.Empty;
 
                 if (opcao == "2" || opcao == "cancelar") break;
 
                 else if (opcao == "1" || opcao == "acessar")
                 {
-                    Console.WriteLine("Digite seu email\n");
-                    string email = Console.ReadLine();
+                    
+                    do
+                    {
+                        
+                        Console.WriteLine("Digite seu email\n");
+                        string email = Console.ReadLine();
 
-                    Console.WriteLine("Digite sua senha\n");
-                    string senha = Console.ReadLine();
+                        Console.WriteLine("Digite sua senha\n");
+                        string senha = Console.ReadLine();
 
+                        bool sucesso = true; //completar com a chamda de uma funcao 
 
+                        if (sucesso == true)
+                        {
+                            Console.WriteLine("Escolha uma opcao:\n");
+                            Console.WriteLine("1 - deslogar\n");
+                            Console.WriteLine("2 - encerrar sistema\n");
+
+                            opcao2 = Console.ReadLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Senha ou usuario invalido\n");
+                            opcao2 = "invalido";
+                        }
+                    } while (opcao2 == "invalido");
 
                 }
 
@@ -32,6 +52,9 @@
                 {
                     Console.WriteLine("opcao invalida");
                 }
+
+                if (opcao2 == "encerrar sistema" || opcao2 == "2") break;
+
 
             }
         }
